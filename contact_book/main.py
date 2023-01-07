235b0c6dc74e14da9388a7c3a167aacd1fbe08b8
Prajakt <prajaktatayde26@gmail.com>
2023-01-07 15:48:43 +0530

    Run all methods from classes of program

--- contact_book/main.py
+++ contact_book/main.py
@@ -13,19 +13,24 @@ def register_contactbook(act):
     act : int
         action depend on user choice
     """
-    con = ContactBook()
-    "Specifying user choice"
-    act.register(1, con.add_contact, "Enter contact details")
-    act.register(2, con.display_all, "Display all contact details")
-    act.register(3, con.search_contact, "Search contact details")
-    act.register(4, con.serialize, "Save contact details")
-    act.register(5, con.deserialize, "Read contact details")
-    act.register(6, con.remove_contact, "Remove contact details")
-    act.register(7, sys.exit, "Exit")
-
+    try:
+        con = ContactBook()
+        "Specifying user choice"
+        act.register(1, con.add_contact, "Enter contact details")
+        act.register(2, con.display_all, "Display all contact details")
+        act.register(3, con.search_contact, "Search contact details")
+        act.register(4, con.serialize, "Save contact details")
+        act.register(5, con.deserialize, "Read contact details")
+        act.register(6, con.remove_contact, "Remove contact details")
+        act.register(7, sys.exit, "Exit")
+    except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     "Creating class object and calling functions"
-    act = Actions()
-    register_contactbook(act)
-    act.run()
+    try:
+        act = Actions()
+        register_contactbook(act)
+        act.run()
+    except Exception as e:
+            print(e)
