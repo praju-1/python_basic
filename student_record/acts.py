dd2f72d8cedb33a8cd7783c0207b7e83ea9e114c
Prajakt <prajaktatayde26@gmail.com>
2023-01-08 21:53:39 +0530

    update as modular

--- student_record/acts.py
+++ student_record/acts.py
@@ -11,25 +11,37 @@ class Actions:
         self.choice = 0
 
     def register(self, key, function, cmd_desc):
-        self.acts[key] = {function:cmd_desc}
+        try:
+            self.acts[key] = {function:cmd_desc}
+        except Exception as e:
+            print(e)
 
     def __str__(self):
-        menu = str("\n----------------------------------\n")
-        for key in self.acts.keys():
-            embed = self.acts.get(key)
-            for desc in embed.values():
-                menu += str(key) + ". " + desc + "\n"
-        return menu
+        try:
+            menu = str("\n----------------------------------\n")
+            for key in self.acts.keys():
+                embed = self.acts.get(key)
+                for desc in embed.values():
+                    menu += str(key) + ". " + desc + "\n"
+            return menu
+        except Exception as e:
+            print(e)
 
     def userchoice(self):
-        self.choice=int(input(" Please Enter your choice :- "))
+        try:
+            self.choice=int(input(" Please Enter your choice :- "))
+        except Exception as e:
+            print(e)
 
     def run(self):
-        while True:
-            # print options / menu
-            print (self)
-            self.userchoice()
-            # call choiced act
-            work = self.acts.get(self.choice)
-            if work != None:
-                list(work.keys())[0]()
+        try:
+            while True:
+                # print options / menu
+                print (self)
+                self.userchoice()
+                # call choiced act
+                work = self.acts.get(self.choice)
+                if work != None:
+                    list(work.keys())[0]()
+        except Exception as e:
+            print(e)
