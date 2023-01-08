3e3423f9749dfff1d9a9b53f8edb61884ae5ee36
Prajakt <prajaktatayde26@gmail.com>
2023-01-08 21:54:55 +0530

    Updated

--- student_record/main.py
+++ student_record/main.py
@@ -3,6 +3,7 @@
 """
 Write a program to create a student record with user input provide display and search functionality.
 """
+#Importing data from another file
 import sys
 from classroom import Classroom
 from acts import Actions
@@ -13,18 +14,21 @@ from acts import Actions
 #
 
 def register_classroom(act):
-    cls = Classroom()
-    act.register(1, cls.accept_recs, "Enter Student records")
-    act.register(2, cls.print_recs, "Display Student records")
-    act.register(3, cls.search, "Search Student records")
-    act.register(4, cls.serialize, "Save record")
-    act.register(5, cls.deserialize, "Read records")
-    act.register(6, sys.exit, "Exit")
-#
-# Entrypoint of program
-#
+    try:
+        "Object creation of class"
+        cls = Classroom()
+        #Calling methods of the respective classes
+        act.register(1, cls.accept_recs, "Enter Student records")
+        act.register(2, cls.print_recs, "Display Student records")
+        act.register(3, cls.search, "Search Student records")
+        act.register(4, cls.serialize, "Save record")
+        act.register(5, cls.deserialize, "Read records")
+        act.register(6, sys.exit, "Exit")
+    except Exception as e:
+            print(e)
+
 if __name__ == "__main__":
+    "Execution of program starts from here"
     act = Actions()
     register_classroom(act)
     act.run()
-
