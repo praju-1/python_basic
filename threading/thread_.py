bf95cfafd8e09d0a74b9a2c80b9e04842ef25718
Praju-1 <prajaktatayde26@gmail.com>
2023-07-22 23:23:31 +0530

    Formatted

--- threading/thread_.py
+++ threading/thread_.py
@@ -1,13 +1,13 @@
-'''
-Write a program to create threads
-'''
+'''Write a program to create threads'''
 
 from threading import Thread
 
+# No of thread to be created
 number_of_threads = 10
 
 # Creating generator here
 def even_numbers():
+    """ This function will return even number with its thread"""
     for number in range(50):
         number % 2 == 0
 
@@ -29,4 +29,3 @@ if __name__ == "__main__":
     print("Threads are : ","\n", threads)
 
     print(" End main ")
-
